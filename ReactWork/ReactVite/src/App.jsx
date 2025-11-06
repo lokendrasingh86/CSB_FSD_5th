ac02e5cd7267aacb86e663dc9dd6e73cdd8b0407
lokendrasingh86 <lokendrasinghsps08@gmail.com>
2025-11-07 04:54:28 +0530

    Login Page(Bootstrap) Commit

--- ReactWork/ReactVite/src/App.jsx
+++ ReactWork/ReactVite/src/App.jsx
@@ -1,22 +1,33 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
-import Gallery from './components/gallery'
-import ImageManipulation from './components/ImageManipulation'
-import StateHandling from './components/StateHandling'
+import "./App.css";
+// import Gallery from './components/gallery'
+// import ImageManipulation from './components/ImageManipulation'
+// import StateHandling from './components/StateHandling'
+
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div className='container' >
-      {/* <h2>welcome to react vite</h2>
+    <>
+      {/*<div className='container' >
+       <h2>welcome to react vite</h2>
       <h3 style={{color: 'brown'}}>
         <Gallery/>
-      </h3> */}
-      {/* <StateHandling></StateHandling> */}
-      <ImageManipulation></ImageManipulation>
-    </div>
-  )
+      </h3> 
+       <StateHandling></StateHandling> 
+       <ImageManipulation></ImageManipulation> 
+    </div> */}
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
